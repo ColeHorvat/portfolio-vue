ebe6ba54c8eaac57f9b87cc1a86f493d89299f06
Cole <ColeHorvat@gmail.com>
2023-03-03 23:43:11 -0800

    Added tailwind support

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,12 @@
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 </script>
-
+ 
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
+    <div class="wrapper bg-red-50">
       <HelloWorld msg="You did it!" />
 
       <nav>
